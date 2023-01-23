a744b8fc740d0889b2007e6eabb963815078ce92
lgtiltda <lgti.ltda@outlook.com>
2023-01-23 10:03:28 -0400

    ATUALIZAR

---
+++ TOMADA-DE-DECISAO-I.py
@@ -0,0 +1,19 @@
+a = 10;
+#TOMADA DE DECISÃO TIPO 'IF' - DECISÃO ÚNICA
+if(a==10):
+    print('A DECISÃO É VERDADEIRA');
+
+#TOMADA DE DECISÃO TIPO 'IF' - DECISÃO DUPLA
+if(a==9):
+    print('A DECISÃO É FALSA');
+else:
+    print('A DECISÃO É VERDADEIRA');
+
+# TOMADA DE DECISÃO TIPO 'IF' - DECISÃO MULTIPLAS
+if (a == 8):
+ print('A DECISÃO É FALSA');
+elif(a == 9):
+ print('A DECISÃO É FALSA');
+elif(a == 10):
+ print('A DECISÃO É VERDADEIRA');
+
